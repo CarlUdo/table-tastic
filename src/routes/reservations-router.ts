4bb3755f24c6e9a46d5fbea6dcefecc72621fbf3
CarlUdo <carl@wbsweden.com>
2024-11-01 20:42:50 +0100

    Try reservations router

--- src/routes/reservations-router.ts
+++ src/routes/reservations-router.ts
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { reservationsDb as db } from "../db/reservations/reservations";
 
 export const createReservationsRouter = (db) => {
   return {
     getRouter: () => {
       const router = express.Router();
+      
+      router.get("/", (req: Request, res: Response) => {
+        res.status(200).json("Hello from Menues router");
+      });
 
       return router;
     },
