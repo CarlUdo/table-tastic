6a62dd6773bb1e8a11cd95f456287a4e2b2cc63e
CarlUdo <carl@wbsweden.com>
2024-11-01 19:15:05 +0100

    Init project

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      semi: "warn",
+      eqeqeq: "warn",
+      "no-unused-vars": "warn",
+      "prefer-const": "warn",
+    },
+  },
+];
