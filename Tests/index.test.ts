6df0fea3f75e0894c71f323081095b96e83c8459
CarlUdo <carl@wbsweden.com>
2024-11-01 19:33:05 +0100

    Set up a template test file

---
+++ Tests/index.test.ts
@@ -0,0 +1,7 @@
+import test from "node:test";
+import request from "supertest";
+import { deepEqual, equal } from "node:assert/strict";
+
+test("GET /status", async () => {
+  equal(true, true);
+});
