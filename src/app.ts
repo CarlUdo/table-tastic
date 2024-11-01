68983a23703597c04843a88c9fcc528764b53ecf
CarlUdo <carl@wbsweden.com>
2024-11-01 20:33:55 +0100

    Import routers and add routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from "express";
+import { createMenuesRouter } from "./routes/menues-router";
+import { createReservationsRouter } from "./routes/reservations-router";
+import { menuesDb } from "./db/menues/menues-db";
+import { reservationsDb } from "./db/reservations/reservations";
 
 export const createApp = () => {
+  const menuesRouter = createMenuesRouter(menuesDb).getRouter();
+  const reservationsRouter =
+    createReservationsRouter(reservationsDb).getRouter();
+
   const app = express();
 
   app.use(express.json());
@@ -8,6 +16,10 @@ export const createApp = () => {
   app.get("/status", (req: Request, res: Response) => {
     res.status(200).json({ message: "Server is up and running!" });
   });
+
+  app.use("/api/v1/menues", menuesRouter);
+
+  app.use("/api/vi/reservations", reservationsRouter);
 
   return app;
 };
