de2b219cc9911f5dd1d75efe73973585a1b4e575
CarlUdo <carl@wbsweden.com>
2024-11-01 20:43:03 +0100

    Try menues router

--- src/routes/menues-router.ts
+++ src/routes/menues-router.ts
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { menuesDb as db } from "../db/menues/menues-db";
 
 export const createMenuesRouter = (db) => {
   return {
     getRouter: () => {
       const router = express.Router();
+      
+      router.get("/", (req: Request, res: Response) => {
+        res.status(200).json("Hello from Menues router");
+      });
 
       return router;
     },
