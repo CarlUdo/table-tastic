8e6f296152a6bfd44cd7acb74bc5000a2c76b987
CarlUdo <carl@wbsweden.com>
2024-11-02 09:24:54 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express, { Request, Response } from "express";
 import { createMenuesRouter } from "./routes/menues-router";
 import { createReservationsRouter } from "./routes/reservations-router";
-import { menuesDb } from "./db/menues/menues-db";
-import { reservationsDb } from "./db/reservations/reservations";
 
 export const createApp = () => {
-  const   menuesRouter = createMenuesRouter(menuesDb).getRouter();
-  const reservationsRouter =
-    createReservationsRouter(reservationsDb).getRouter();
+  const menuesRouter = createMenuesRouter();
+  const reservationsRouter = createReservationsRouter();
 
   const app = express();
 
