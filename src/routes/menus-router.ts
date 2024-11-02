752922676728e7f403617d29f1dffdf3de773203
CarlUdo <carl@wbsweden.com>
2024-11-02 18:45:06 +0100

    Make route for updating a menu

--- src/routes/menus-router.ts
+++ src/routes/menus-router.ts
@@ -1,5 +1,10 @@
 import express from "express";
-import { addMenu, getAllMenus, getMenu } from "../controllers/menus-controller";
+import {
+  addMenu,
+  getAllMenus,
+  getMenu,
+  updateMenu,
+} from "../controllers/menus-controller";
 
 export const createMenusRouter = () => {
   const router = express.Router();
@@ -7,6 +12,7 @@ export const createMenusRouter = () => {
   router.get("/", getAllMenus);
   router.get("/:id", getMenu);
   router.post("/", addMenu);
+  router.patch("/:id", updateMenu);
 
   return router;
 };
