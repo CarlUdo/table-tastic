54d92c427753b9ca5ac899ba6dfe5b6ef9fedcf1
CarlUdo <carl@wbsweden.com>
2024-11-02 10:08:00 +0100

    Make hard-coded uuid:s

--- src/db/menues/menues-db.ts
+++ src/db/menues/menues-db.ts
@@ -1,18 +1,16 @@
-import { v4 as uuidv4 } from 'uuid';
-
 export const menuesDb = [
   {
-    id: uuidv4(),
+    id: "9de3faf7-36f3-4449-b4b5-7c3393f00e10",
     name: "Breakfast Menu",
     items: ["Pancakes", "Omelette"],
   },
   {
-    id: uuidv4(),
+    id: "fd9c2bf1-8540-4ed9-9be7-155877262259",
     name: "Lunch Menu",
     items: ["Burger", "Salad"],
   },
   {
-    id: uuidv4(),
+    id: "42995559-2641-4d33-85e9-9043373fc6bf",
     name: "Dinner Menu",
     items: ["Steak", "Pasta"],
   },
