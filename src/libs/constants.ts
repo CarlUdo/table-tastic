c501d088f1706e4c93c6ed25c71607ccf6f29b59
CarlUdo <carl@wbsweden.com>
2024-11-02 15:31:49 +0100

    Make string constant for menu exists

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -3,7 +3,8 @@ export const MENUS_BASE_URL = "/api/v1/menus";
 export const RESERVATIONS_BASE_URL = "/api/v1/reservations";
 
 /* Error messages */
-export const GENERAL_SERVER_ERROR = "Something went wrong";
-export const MENU_NOT_FOUND = "Menu not found";
-export const INVALID_ID = "Invalid ID format";
-export const INVALID_MENU = "Invalid menu format";
+export const GENERAL_SERVER_ERROR = "Something went wrong.";
+export const MENU_NOT_FOUND = "Menu not found.";
+export const INVALID_ID = "Invalid ID format.";
+export const INVALID_MENU = "Invalid menu format.";
+export const MENU_EXISTS = "Menu already exixts.";
