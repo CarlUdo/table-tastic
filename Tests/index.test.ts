9148c6440917e11c6cef78b39ed908df1a702f64
CarlUdo <carl@wbsweden.com>
2024-11-02 12:44:06 +0100

    Make test "GET all menus includes breakfast menu"

--- Tests/index.test.ts
+++ Tests/index.test.ts
@@ -3,6 +3,7 @@ import request from "supertest";
 import { deepEqual, equal } from "node:assert/strict";
 import { createApp } from "../src/app";
 import { menusBaseUrl } from "../src/libs/constants";
+import type { Menu } from "../src/db/menus/menus-db";
 
 test("GET /status", async () => {
   const app = createApp();
@@ -27,3 +28,20 @@ test("GET all menus returns 3 menus", async () => {
 
   equal(response.body.length, 3);
 });
+
+test("GET all menus includes breakfast menu", async () => {
+  const app = createApp();
+
+  const breakfastMenu: Menu = {
+    id: "9de3faf7-36f3-4449-b4b5-7c3393f00e10",
+    name: "Breakfast Menu",
+    dishes: ["Pancakes", "Omelette"],
+  };
+
+  const responseBody: Menu[] = (await request(app).get(menusBaseUrl)).body;
+
+  const breakfastMenuExists = responseBody.some(menu => JSON.stringify(menu) === JSON.stringify(breakfastMenu));
+
+  equal(true, breakfastMenuExists);
+});
+
