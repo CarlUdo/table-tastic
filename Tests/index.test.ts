8408c93c5e1b86532350542f3eab0ee1ef41c39e
CarlUdo <carl@wbsweden.com>
2024-11-02 12:55:39 +0100

    Make test "GET menu id '42995559-2641-4d33-85e9-9043373fc6bf' should return dinner menu"

--- Tests/index.test.ts
+++ Tests/index.test.ts
@@ -2,7 +2,7 @@ import test from "node:test";
 import request from "supertest";
 import { deepEqual, equal } from "node:assert/strict";
 import { createApp } from "../src/app";
-import { menusBaseUrl } from "../src/libs/constants";
+import { MENUS_BASE_URL } from "../src/libs/constants";
 import type { Menu } from "../src/db/menus/menus-db";
 
 test("GET /status", async () => {
@@ -16,7 +16,7 @@ test("GET /status", async () => {
 test("GET all menus returns 200 status", async () => {
   const app = createApp();
 
-  const response = await request(app).get(menusBaseUrl);
+  const response = await request(app).get(MENUS_BASE_URL);
 
   equal(response.status, 200);
 });
@@ -24,7 +24,7 @@ test("GET all menus returns 200 status", async () => {
 test("GET all menus returns 3 menus", async () => {
   const app = createApp();
 
-  const response = await request(app).get(menusBaseUrl);
+  const response = await request(app).get(MENUS_BASE_URL);
 
   equal(response.body.length, 3);
 });
@@ -38,10 +38,24 @@ test("GET all menus includes breakfast menu", async () => {
     dishes: ["Pancakes", "Omelette"],
   };
 
-  const responseBody: Menu[] = (await request(app).get(menusBaseUrl)).body;
+  const responseBody: Menu[] = (await request(app).get(MENUS_BASE_URL)).body;
 
   const breakfastMenuExists = responseBody.some(menu => JSON.stringify(menu) === JSON.stringify(breakfastMenu));
 
   equal(true, breakfastMenuExists);
+});
+
+test("GET menu id '42995559-2641-4d33-85e9-9043373fc6bf' should return dinner menu", async () => {
+  const app = createApp();
+
+  const dinnerMenu: Menu = {
+    id: "42995559-2641-4d33-85e9-9043373fc6bf",
+    name: "Dinner Menu",
+    dishes: ["Steak", "Pasta"],
+  };
+
+  const responseBody: Menu[] = (await request(app).get(`${MENUS_BASE_URL}/42995559-2641-4d33-85e9-9043373fc6bf`)).body;
+
+  deepEqual(responseBody, dinnerMenu);
 });
 
