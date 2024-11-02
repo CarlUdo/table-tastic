903c44fe6fdec855513f4ad83ebb72684711c9de
CarlUdo <carl@wbsweden.com>
2024-11-02 09:45:54 +0100

    Make a route for getting menu by id

--- src/routes/menues-router.ts
+++ src/routes/menues-router.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllMenues } from "../controllers/menues-controller";
+import { getAllMenues, getMenu } from "../controllers/menues-controller";
 
 export const createMenuesRouter = () => {
   const router = express.Router();
 
   router.get("/", getAllMenues);
+  router.get("/:id", getMenu);
 
   return router;
 };
