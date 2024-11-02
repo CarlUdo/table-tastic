d166d8419d7dcd367f588a7628b40d4b713f9543
CarlUdo <carl@wbsweden.com>
2024-11-02 13:26:43 +0100

    Make error string for menu not found

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -5,3 +5,4 @@ export const RESERVATIONS_BASE_URL = "/api/v1/reservations";
 /* Error messages */
 export const GENERAL_SERVER_ERROR = "Something went wrong";
 export const INVALID_ID = "Invalid ID format";
+export const MENU_NOT_FOUND = "Menu not found";
