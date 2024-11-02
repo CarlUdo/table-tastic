200a6bebe5d623a30af64872ddaab753420913e1
CarlUdo <carl@wbsweden.com>
2024-11-02 08:52:08 +0100

    Make a menues db

--- src/db/menues/menues-db.ts
+++ src/db/menues/menues-db.ts
@@ -1,1 +1,17 @@
-export const menuesDb = [];
+export const menuesDb = [
+  {
+    id: 1,
+    name: "Breakfast Menu",
+    items: ["Pancakes", "Omelette"],
+  },
+  {
+    id: 2,
+    name: "Lunch Menu",
+    items: ["Burger", "Salad"],
+  },
+  {
+    id: 3,
+    name: "Dinner Menu",
+    items: ["Steak", "Pasta"],
+  },
+];
