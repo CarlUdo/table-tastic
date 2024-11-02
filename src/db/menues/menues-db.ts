713b05f0d53486f619063a2eeb4d5090722441eb
CarlUdo <carl@wbsweden.com>
2024-11-02 11:41:15 +0100

    Add types to menu db

--- src/db/menues/menues-db.ts
+++ src/db/menues/menues-db.ts
@@ -1,17 +1,25 @@
-export const menuesDb = [
+type Menues = "Breakfast Menu" | "Lunch Menu" | "Dinner Menu";
+
+type MenuesDb = {
+  id: string;
+  name: Menues;
+  dishes: string[];
+};
+
+export const menuesDb: MenuesDb[] = [
   {
     id: "9de3faf7-36f3-4449-b4b5-7c3393f00e10",
     name: "Breakfast Menu",
-    items: ["Pancakes", "Omelette"],
+    dishes: ["Pancakes", "Omelette"],
   },
   {
     id: "fd9c2bf1-8540-4ed9-9be7-155877262259",
     name: "Lunch Menu",
-    items: ["Burger", "Salad"],
+    dishes: ["Burger", "Salad"],
   },
   {
     id: "42995559-2641-4d33-85e9-9043373fc6bf",
     name: "Dinner Menu",
-    items: ["Steak", "Pasta"],
+    dishes: ["Steak", "Pasta"],
   },
 ];
