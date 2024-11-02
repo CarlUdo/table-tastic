63bce1fc7375cff6a76e2b8f02841f4752758f25
CarlUdo <carl@wbsweden.com>
2024-11-02 09:23:25 +0100

    Make menu db function getAll()

--- src/services/menues/menues-db-functions.ts
+++ src/services/menues/menues-db-functions.ts
@@ -1,1 +1,11 @@
+/*
+  To mirror real-life scenarios where database operations are asynchronous I utilize promises and async/await syntax.
+*/ 
+
 import { menuesDb } from "../../db/menues/menues-db";
+
+export const getAll = async () => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(menuesDb), 100);
+  });
+};
