5a76ec4cb3877f8f660c487aefc0283939919326
CarlUdo <carl@wbsweden.com>
2024-11-02 09:45:26 +0100

    Make menu db function getById()

--- src/services/menues/menues-db-functions.ts
+++ src/services/menues/menues-db-functions.ts
@@ -1,11 +1,23 @@
 /*
   To mirror real-life scenarios where database operations are asynchronous I utilize promises and async/await syntax.
-*/ 
+*/
 
 import { menuesDb } from "../../db/menues/menues-db";
 
 export const getAll = async () => {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => resolve(menuesDb), 100);
+  });
+};
+
+export const getById = async (id: string) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const menu = menuesDb.find((menu) => menu.id === id);
+
+      if (!menu) reject(new Error("Menu not found"));
+
+      resolve(menu);
+    }, 100);
   });
 };
