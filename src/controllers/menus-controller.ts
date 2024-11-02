4946a349aaee2b514f694febd787f0b25a835a61
CarlUdo <carl@wbsweden.com>
2024-11-02 20:36:24 +0100

    Make menu controller for deleting menu

--- src/controllers/menus-controller.ts
+++ src/controllers/menus-controller.ts
@@ -4,6 +4,7 @@ import {
   getById,
   create,
   update,
+  remove,
 } from "../services/menus/menus-db-functions";
 import { idSchema, menuSchema } from "../validation";
 import {
@@ -117,6 +118,29 @@ export const updateMenu = async (req: Request, res: Response) => {
     }
 
     res.status(200).json(updatedMenu);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: { message: error.message } });
+      return;
+    }
+    res.status(500).json({ error: { message: GENERAL_SERVER_ERROR } });
+  }
+};
+
+export const deleteMenu = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const validationResult = idSchema.safeParse(id);
+
+    if (!validationResult.success) {
+      res.status(400).json({ error: { message: INVALID_ID } });
+      return;
+    }
+
+    const menu = await remove(validationResult.data);
+
+    res.status(200).json(menu);
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ error: { message: error.message } });
