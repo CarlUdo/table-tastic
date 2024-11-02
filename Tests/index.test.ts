63b3c22ee3b95a01a2d13b70281009cb9f73e3db
CarlUdo <carl@wbsweden.com>
2024-11-02 12:25:14 +0100

    Make test "GET all menus returns 3 menus"

--- Tests/index.test.ts
+++ Tests/index.test.ts
@@ -2,6 +2,7 @@ import test from "node:test";
 import request from "supertest";
 import { deepEqual, equal } from "node:assert/strict";
 import { createApp } from "../src/app";
+import { menusBaseUrl } from "../src/libs/constants";
 
 test("GET /status", async () => {
   const app = createApp();
@@ -9,4 +10,20 @@ test("GET /status", async () => {
   const response = await request(app).get("/status");
 
   equal(response.status, 200);
+});
+
+test("GET all menus returns 200 status", async () => {
+  const app = createApp();
+
+  const response = await request(app).get(menusBaseUrl);
+
+  equal(response.status, 200);
+});
+
+test("GET all menus returns 3 menus", async () => {
+  const app = createApp();
+
+  const response = await request(app).get(menusBaseUrl);
+
+  equal(response.body.length, 3);
 });
