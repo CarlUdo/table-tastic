463d9fbcb743a43a9d94430a5580110fd77d225f
CarlUdo <carl@wbsweden.com>
2024-11-02 09:29:18 +0100

    Convert ids to strings

--- src/db/menues/menues-db.ts
+++ src/db/menues/menues-db.ts
@@ -1,16 +1,16 @@
 export const menuesDb = [
   {
-    id: 1,
+    id: "1",
     name: "Breakfast Menu",
     items: ["Pancakes", "Omelette"],
   },
   {
-    id: 2,
+    id: "2",
     name: "Lunch Menu",
     items: ["Burger", "Salad"],
   },
   {
-    id: 3,
+    id: "3",
     name: "Dinner Menu",
     items: ["Steak", "Pasta"],
   },
