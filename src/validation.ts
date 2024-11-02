1e6500b9f182e0cf66b68f6bc093ef4c8fef626e
CarlUdo <carl@wbsweden.com>
2024-11-02 14:25:24 +0100

    Make validation schema for menu

--- src/validation.ts
+++ src/validation.ts
@@ -1,3 +1,10 @@
 import { z } from "zod";
 
 export const idSchema = z.string().uuid();
+
+const menuNameSchema = z.enum(["Breakfast Menu", "Lunch Menu", "Dinner Menu"]);
+
+export const menuSchema = z.object({
+  name: menuNameSchema,
+  dishes: z.array(z.string()),
+});
