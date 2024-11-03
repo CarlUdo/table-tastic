a4e79e09d7d2496862073119815e1365cd5b9f00
CarlUdo <carl@wbsweden.com>
2024-11-03 12:01:59 +0100

    Make reservations controller update reservation

--- src/controllers/reservations-controller.ts
+++ src/controllers/reservations-controller.ts
@@ -9,9 +9,10 @@ import {
 } from "../libs/constants";
 import { v4 as uuidv4 } from "uuid";
 import { getMenuId } from "../services/menus/menus-db-helper-functions";
-import { create, getById } from "../services/reservations/reservations-db-functions";
+import { create, getById, update } from "../services/reservations/reservations-db-functions";
 import { getAll } from "../services/reservations/reservations-db-functions";
 import { idSchema } from "../validation/id.schema";
+import { Reservation } from "../db/reservations/reservations-db";
 
 export const getAllReservations = async (req: Request, res: Response) => {
   try {
@@ -75,12 +76,58 @@ export const makeReservation = async (req: Request, res: Response) => {
       id: reservationId,
       menuId,
     });
-    console.log("Reservation: ", newReservation);
+
+    res.status(201).json(newReservation);
   } catch (error) {
     if (error instanceof Error) {
       let statusCode = 500;
       if (error.message === FULLY_BOOKED) statusCode = 409;
       res.status(statusCode).json({ error: { message: error.message } });
+      return;
+    }
+    res.status(500).json({ error: { message: GENERAL_SERVER_ERROR } });
+  }
+};
+
+export const updateReservation = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const reservation = req.body;
+
+    const idValidationResult = idSchema.safeParse(id);
+
+    const reservationValidationResult = reservationSchema.safeParse(reservation);
+
+    if (!idValidationResult.success) {
+      res.status(400).json({ error: { message: INVALID_ID } });
+      return;
+    }
+
+    if (!reservationValidationResult.success) {
+      res.status(400).json({ error: { message: INVALID_RESERVATION } });
+      return;
+    }
+
+    const menuId = await getMenuId(reservationValidationResult.data.time);
+
+    if (!menuId) {
+      res.status(400).json({ error: { message: NO_MENU } });
+      return;
+    }
+
+    const reservationToUpdate: Reservation = {
+      id: idValidationResult.data,
+      ...reservationValidationResult.data,
+      menuId
+    };
+
+    const updatedReservation = await update(reservationToUpdate);
+
+    res.status(200).json(updatedReservation);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: { message: error.message } });
       return;
     }
     res.status(500).json({ error: { message: GENERAL_SERVER_ERROR } });
