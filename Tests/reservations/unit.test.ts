f644dce9736693eecae21c7f3e3bc531d435c112
CarlUdo <carl@wbsweden.com>
2024-11-03 15:26:26 +0100

    Make reservations unit test "create should reject if reservation is fully booked"

--- Tests/reservations/unit.test.ts
+++ Tests/reservations/unit.test.ts
@@ -1,9 +1,9 @@
 import test from "node:test";
 import { deepEqual } from "node:assert/strict";
-import { getAll, getById } from "../../src/services/reservations/reservations-db-functions";
-import { reservationsDb as db } from "../../src/db/reservations/reservations-db";
+import { create, getAll, getById } from "../../src/services/reservations/reservations-db-functions";
+import { reservationsDb as db, Reservation } from "../../src/db/reservations/reservations-db";
 import { rejects } from "node:assert";
-import { RESERVATION_NOT_FOUND } from "../../src/libs/constants";
+import { FULLY_BOOKED, RESERVATION_NOT_FOUND } from "../../src/libs/constants";
 
 test("getAll should return all reservations", async () => {
   const result = await getAll();
@@ -24,4 +24,19 @@ test("getById should reject if reservation not found", async () => {
   await rejects(async () => {
     await getById(invalidId);
   }, new Error(RESERVATION_NOT_FOUND));
+});
+
+test("create should reject if reservation is fully booked", async () => {
+  const newReservation: Reservation = {
+    id: "new-id",
+    customerName: "Marilyn vos Savant",
+    tableNumber: 1,
+    date: "2024-11-23",
+    time: "08:00",
+    menuId: "9de3faf7-36f3-4449-b4b5-7c3393f00e10",
+  };
+
+  await rejects(async () => {
+    await create (newReservation);
+  }, new Error(FULLY_BOOKED));
 });
