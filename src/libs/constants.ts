af5d9589cf8bd89182d70e81162dd8a732d65d92
CarlUdo <carl@wbsweden.com>
2024-11-03 11:15:39 +0100

    Add constants

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -8,3 +8,6 @@ export const MENU_NOT_FOUND = "Menu not found.";
 export const INVALID_ID = "Invalid ID format.";
 export const INVALID_MENU = "Invalid menu format.";
 export const MENU_EXISTS = "Menu already exists.";
+export const INVALID_RESERVATION = "Invalid reservation format.";
+export const FULLY_BOOKED = "Restaurant is fully booked.";
+export const NO_MENU = "No menu available for the given time.";
