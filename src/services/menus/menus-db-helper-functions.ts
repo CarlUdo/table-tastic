e74b719f45b020fc5046800b6f087268fefeed35
CarlUdo <carl@wbsweden.com>
2024-11-03 09:40:35 +0100

    Make helper function to get Menu id regards to time

---
+++ src/services/menus/menus-db-helper-functions.ts
@@ -0,0 +1,13 @@
+import { getAll } from "./menus-db-functions";
+
+export const getMenuId = async (time: string) => {
+  const [hour] = time.split(":").map(Number);
+  const menus = await getAll();
+
+  if (hour >= 0 && hour < 10)
+    return menus.find((menu) => menu.name === "Breakfast Menu")?.id;
+  if (hour >= 10 && hour < 16)
+    return menus.find((menu) => menu.name === "Lunch Menu")?.id;
+
+  return menus.find((menu) => menu.name === "Dinner Menu")?.id;
+};
