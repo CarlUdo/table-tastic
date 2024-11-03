aadefad1b41232f09c12b0a1e589042cd6324bb0
CarlUdo <carl@wbsweden.com>
2024-11-03 11:08:18 +0100

    Make reservations function for making a reservation

--- src/services/reservations/reservations-db-functions.ts
+++ src/services/reservations/reservations-db-functions.ts
@@ -2,4 +2,22 @@
   To mirror real-life scenarios where database operations are asynchronous I utilize promises and async/await syntax.
 */
 
-import { reservationsDb } from "../../db/reservations/reservations";
+import type { Reservation } from "../../db/reservations/reservations-db";
+import { reservationsDb } from "../../db/reservations/reservations-db";
+import { FULLY_BOOKED } from "../../libs/constants";
+
+export const create = async (reservation: Reservation): Promise<Reservation> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const fullyBooked = reservationsDb.some((dbReservation) => dbReservation.date === reservation.date && dbReservation.time === reservation.time);
+
+      if (fullyBooked) {
+        reject(new Error(FULLY_BOOKED));
+        return;
+      }
+
+      reservationsDb.push(reservation);
+      resolve(reservation);
+    }, 100);
+  });
+};
