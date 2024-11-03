39a3c73485047c44a0da9834975c5f5beeae2d01
CarlUdo <carl@wbsweden.com>
2024-11-03 15:21:40 +0100

    Make reservations unit test "getById should return the correct reservation by id"

--- Tests/reservations/unit.test.ts
+++ Tests/reservations/unit.test.ts
@@ -1,9 +1,17 @@
 import test from "node:test";
 import { deepEqual } from "node:assert/strict";
-import { getAll } from "../../src/services/reservations/reservations-db-functions";
+import { getAll, getById } from "../../src/services/reservations/reservations-db-functions";
 import { reservationsDb as db } from "../../src/db/reservations/reservations-db";
 
 test("getAll should return all reservations", async () => {
   const result = await getAll();
   deepEqual(result, db);
+});
+
+test("getById should return the correct reservation by id", async () => {
+  const validId = "f576ecc3-b655-488a-b83d-dfbd0182ba5d";
+
+  const result = await getById(validId);
+  
+  deepEqual(result, db.find((reservation) => reservation.id === validId));
 });
