9c86c015f7e4bd97a97633dd2925c22a2b2a8d42
CarlUdo <carl@wbsweden.com>
2024-11-03 11:22:17 +0100

    Make reservations route get reservation

--- src/routes/reservations-router.ts
+++ src/routes/reservations-router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getAllReservations,
+  getReservation,
   makeReservation,
 } from "../controllers/reservations-controller";
 
@@ -8,6 +9,7 @@ export const createReservationsRouter = () => {
   const router = express.Router();
 
   router.get("/", getAllReservations);
+  router.get("/:id", getReservation);
   router.post("/", makeReservation);
 
   return router;
