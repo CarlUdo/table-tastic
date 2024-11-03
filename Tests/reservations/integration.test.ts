560708d01c516c9d7ab2e8f416fc0ade4af770c7
CarlUdo <carl@wbsweden.com>
2024-11-03 15:55:13 +0100

    Make reservations integration test "GET reservation by id should return the correct reservation"

--- Tests/reservations/integration.test.ts
+++ Tests/reservations/integration.test.ts
@@ -13,6 +13,19 @@ test("GET all reservations should return all reservations", async () => {
   const responseBody = response.body;
 
   equal(response.status, 200);
-  
+
   deepEqual(responseBody, db);
+});
+
+test("GET reservation by id should return the correct reservation", async () => {
+  const app = createApp();
+
+  const validId = "f576ecc3-b655-488a-b83d-dfbd0182ba5d";
+
+  const response = await request(app).get(`${RESERVATIONS_BASE_URL}/${validId}`);
+
+  const expectedReservation = db.find(res => res.id === validId);
+
+  equal(response.status, 200);
+  deepEqual(response.body, expectedReservation);
 });
