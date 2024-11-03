86215403063cdecf5a3a534b3f8898f788b9bdf7
CarlUdo <carl@wbsweden.com>
2024-11-03 17:38:58 +0100

    Format code

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 export const logger = (req: Request, res: Response, next: NextFunction) => {
   const now = new Date();
-  
+
   console.log(`[${now.toISOString()}] ${req.method} ${req.url}`);
-  
+
   next();
 };
