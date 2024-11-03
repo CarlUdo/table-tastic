19276214dd37ce0f8ef5f65d20d7be11aef1e0c6
CarlUdo <carl@wbsweden.com>
2024-11-03 15:23:05 +0100

    Make reservations unit test "getById should reject if reservation not found"

--- Tests/reservations/unit.test.ts
+++ Tests/reservations/unit.test.ts
@@ -2,6 +2,8 @@ import test from "node:test";
 import { deepEqual } from "node:assert/strict";
 import { getAll, getById } from "../../src/services/reservations/reservations-db-functions";
 import { reservationsDb as db } from "../../src/db/reservations/reservations-db";
+import { rejects } from "node:assert";
+import { RESERVATION_NOT_FOUND } from "../../src/libs/constants";
 
 test("getAll should return all reservations", async () => {
   const result = await getAll();
@@ -14,4 +16,12 @@ test("getById should return the correct reservation by id", async () => {
   const result = await getById(validId);
   
   deepEqual(result, db.find((reservation) => reservation.id === validId));
+});
+
+test("getById should reject if reservation not found", async () => {
+  const invalidId = "invalid-id";
+
+  await rejects(async () => {
+    await getById(invalidId);
+  }, new Error(RESERVATION_NOT_FOUND));
 });
