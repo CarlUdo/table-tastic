7f183a0f3d1cd3138fdacb6cc17e2ee279ed0493
CarlUdo <carl@wbsweden.com>
2024-11-04 17:33:06 +0100

    Make a create menu database factory function

--- src/services/menus/menus-db-functions.ts
+++ src/services/menus/menus-db-functions.ts
@@ -5,73 +5,142 @@
 import { menusDb, Menu } from "../../db/menus";
 import { MENU_EXISTS, MENU_NOT_FOUND } from "../../libs";
 
-export const getAll = async (): Promise<Menu[]> => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(menusDb), 100);
-  });
+export const createMenusDb =  {
+  getAll: async (): Promise<Menu[]> => {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(menusDb), 100);
+    });
+  },
+  getById: async (id: string): Promise<Menu> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const menu = menusDb.find((dbMenu) => dbMenu.id === id);
+  
+        if (!menu) {
+          reject(new Error(MENU_NOT_FOUND));
+          return;
+        }
+  
+        resolve(menu);
+      }, 100);
+    });
+  },
+  create: async (menu: Menu): Promise<Menu> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const exists = menusDb.some((dbMenu) => dbMenu.name === menu.name);
+  
+        if (exists) {
+          reject(new Error(MENU_EXISTS));
+          return;
+        }
+  
+        menusDb.push(menu);
+        resolve(menu);
+      }, 100);
+    });
+  },
+  update: async (menu: Menu): Promise<Menu | ""> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const menuToUpdate = menusDb.find((dbMenu) => dbMenu.id === menu.id);
+  
+        if (!menuToUpdate) {
+          reject(new Error(MENU_NOT_FOUND));
+          return;
+        }
+  
+        resolve(menu);
+      }, 100);
+    });
+  },
+  remove: async (id: string): Promise<Menu> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const menu = menusDb.find((dbMenu) => dbMenu.id === id);
+        if (!menu) {
+          reject(new Error(MENU_NOT_FOUND));
+          return;
+        }
+  
+        const updatedMenusDb = menusDb.filter((dbMenu) => dbMenu.id !== id);
+  
+        menusDb.length = 0;
+        menusDb.push(...updatedMenusDb);
+  
+        resolve(menu);
+      }, 100);
+    });
+  }
 };
 
-export const getById = async (id: string): Promise<Menu> => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const menu = menusDb.find((dbMenu) => dbMenu.id === id);
+// export const getAll = async (): Promise<Menu[]> => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(menusDb), 100);
+//   });
+// };
 
-      if (!menu) {
-        reject(new Error(MENU_NOT_FOUND));
-        return;
-      }
+// export const getById = async (id: string): Promise<Menu> => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const menu = menusDb.find((dbMenu) => dbMenu.id === id);
 
-      resolve(menu);
-    }, 100);
-  });
-};
+//       if (!menu) {
+//         reject(new Error(MENU_NOT_FOUND));
+//         return;
+//       }
 
-export const create = async (menu: Menu): Promise<Menu> => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const exists = menusDb.some((dbMenu) => dbMenu.name === menu.name);
+//       resolve(menu);
+//     }, 100);
+//   });
+// };
 
-      if (exists) {
-        reject(new Error(MENU_EXISTS));
-        return;
-      }
+// export const create = async (menu: Menu): Promise<Menu> => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const exists = menusDb.some((dbMenu) => dbMenu.name === menu.name);
 
-      menusDb.push(menu);
-      resolve(menu);
-    }, 100);
-  });
-};
+//       if (exists) {
+//         reject(new Error(MENU_EXISTS));
+//         return;
+//       }
 
-export const update = async (menu: Menu): Promise<Menu | ""> => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const menuToUpdate = menusDb.find((dbMenu) => dbMenu.id === menu.id);
+//       menusDb.push(menu);
+//       resolve(menu);
+//     }, 100);
+//   });
+// };
 
-      if (!menuToUpdate) {
-        reject(new Error(MENU_NOT_FOUND));
-        return;
-      }
+// export const update = async (menu: Menu): Promise<Menu | ""> => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const menuToUpdate = menusDb.find((dbMenu) => dbMenu.id === menu.id);
 
-      resolve(menu);
-    }, 100);
-  });
-};
+//       if (!menuToUpdate) {
+//         reject(new Error(MENU_NOT_FOUND));
+//         return;
+//       }
 
-export const remove = async (id: string): Promise<Menu> => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const menu = menusDb.find((dbMenu) => dbMenu.id === id);
-      if (!menu) {
-        reject(new Error(MENU_NOT_FOUND));
-        return;
-      }
+//       resolve(menu);
+//     }, 100);
+//   });
+// };
 
-      const updatedMenusDb = menusDb.filter((dbMenu) => dbMenu.id !== id);
+// export const remove = async (id: string): Promise<Menu> => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const menu = menusDb.find((dbMenu) => dbMenu.id === id);
+//       if (!menu) {
+//         reject(new Error(MENU_NOT_FOUND));
+//         return;
+//       }
 
-      menusDb.length = 0;
-      menusDb.push(...updatedMenusDb);
+//       const updatedMenusDb = menusDb.filter((dbMenu) => dbMenu.id !== id);
 
-      resolve(menu);
-    }, 100);
-  });
-};
+//       menusDb.length = 0;
+//       menusDb.push(...updatedMenusDb);
+
+//       resolve(menu);
+//     }, 100);
+//   });
+// };
