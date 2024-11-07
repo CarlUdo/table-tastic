1fc6a206b6478a51e8d402ed2c240e962628c719
CarlUdo <carl@wbsweden.com>
2024-11-07 18:56:21 +0100

    Make menu repository

---
+++ src/features/menus/repository.ts
@@ -0,0 +1,35 @@
+import { Menu } from "./validation";
+
+export const createRepository = () => {
+  const menusDb: Menu[] = [
+    {
+      id: "9de3faf7-36f3-4449-b4b5-7c3393f00e10",
+      name: "Breakfast Menu",
+      dishes: ["Pancakes", "Omelette"],
+    },
+    {
+      id: "fd9c2bf1-8540-4ed9-9be7-155877262259",
+      name: "Lunch Menu",
+      dishes: ["Burger", "Salad"],
+    },
+    {
+      id: "42995559-2641-4d33-85e9-9043373fc6bf",
+      name: "Dinner Menu",
+      dishes: ["Steak", "Pasta"],
+    },
+  ];
+
+  return {
+    getAll: async () => menusDb,
+    
+   /*  getById: async (id: string) =>  menusDb.find((dbMenu) => dbMenu.id === id),   */    
+    
+    create: async (menu: Menu) => menusDb.push(menu),  
+
+    update: async (index: number, update: any) => menusDb[index] = { ...menusDb[index], ...update },
+
+    remove: async (id: string) => menusDb.filter(menu => menu.id !== id),
+  };
+};
+
+export type Repository = ReturnType<typeof createRepository>;
