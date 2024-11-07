17db6689176dda34b431a3eda054cfc6af253a3f
CarlUdo <carl@wbsweden.com>
2024-11-07 18:55:36 +0100

    Update menu validation schema

--- src/features/menus/validation/index.schema.ts
+++ src/features/menus/validation/index.schema.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
-export const idSchema = z.string().uuid();
+export const idSchema = z.object({
+  id: z.string().uuid(),
+});
 
 const menuNameSchema = z.enum(["Breakfast Menu", "Lunch Menu", "Dinner Menu"]);
 
@@ -9,4 +11,6 @@ export const menuSchema = z.object({
   dishes: z.array(z.string()),
 });
 
-export type Menu = z.infer<typeof idSchema> | z.infer<typeof menuSchema>;
+const fullMenuSchema = menuSchema.merge(idSchema);
+
+export type Menu = z.infer<typeof fullMenuSchema>;
