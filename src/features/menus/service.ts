637bc9dc9457f89302b45401061b922543cc1532
CarlUdo <carl@wbsweden.com>
2024-11-08 20:00:55 +0100

    Update menu service file

--- src/features/menus/service.ts
+++ src/features/menus/service.ts
@@ -6,6 +6,7 @@ import {
 } from "./validation";
 import { Repository } from ".";
 import { v4 } from "uuid";
+import { DuplicateKeyError, NotFoundError } from "../../errors";
 
 export const createService = (db: Repository) => {
   return {
@@ -13,20 +14,23 @@ export const createService = (db: Repository) => {
 
     getMenu: async (id: string) => {
       const menu = await db.getById(id);
-      if (!menu) throw new Error("Menu not found");
+      if (!menu) throw new NotFoundError("Menu not found");
       return menu;
     },
 
     addMenu: async (rawData: NewMenu) => {
       const parsedMenu = newMenuSchema.parse(rawData);
+      const exists = (await db.getAll()).some((dbMenu) => dbMenu.name === parsedMenu.name);
+      if (exists) throw new DuplicateKeyError("Menu name already exists.");
       db.create({ id: v4(), ...parsedMenu });
     },
 
     updateMenu: async (rawData: MenuUpdates, id: string) => {
-      const parsedUpdates = menuUpdatesSchema.parse(rawData);
+      //const parsedUpdates = menuUpdatesSchema.parse(rawData);
+      console.log("Parsed updates: ", rawData);
       const index = (await db.getAll()).findIndex((menu) => menu.id === id);
       if (index === -1) throw new Error("Menu not found");
-      db.update(parsedUpdates, index);
+      return await db.update(rawData, index);
     },
 
     removeMenu: async (id: string) => {
