b684411ed819a1fff1e055c42d68002177ad0aca
CarlUdo <carl@wbsweden.com>
2024-11-08 19:38:59 +0100

    Update error handler

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,25 +1,24 @@
 import { ErrorRequestHandler } from "express";
 import { ZodError } from "zod";
-import { AppError } from "../errors";
-export function createErrorRequestHandler() {
-  const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+import { BadRequestError, NotFoundError } from "../errors";
+
+export const createErrorRequestHandler = (): ErrorRequestHandler => {
+  return (err, _req, res, _next) => {
     if (err instanceof ZodError) {
       res.status(400).json(err.errors);
       return;
     }
 
-    if (err instanceof AppError) {
-      if (err.statusMessage() === "NOT_FOUND_ERROR") {
-        res.status(404).json(err.message);
-        return;
-      }
-      if (err.statusMessage() === "BAD_REQUEST_ERROR") {
-        res.status(500).json(err.message);
-        return;
-      }
-    } else {
-      res.status(500).json({ message: "Internal server error" });
+    if (err instanceof NotFoundError) {
+      res.status(err.statusCode).json({ error: err.message });
+      return;
     }
+
+    if (err instanceof BadRequestError) {
+      res.status(err.statusCode).json({ error: err.message });
+      return;
+    }
+
+    res.status(500).json({ error: "Internal server error" });
   };
-  return errorHandler;
-}
+};
