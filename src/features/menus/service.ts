05acf368f162ed242db0fbae712acf63454eb560
CarlUdo <carl@wbsweden.com>
2024-11-09 09:25:34 +0100

    Make updateMenu() service function

--- src/features/menus/service.ts
+++ src/features/menus/service.ts
@@ -1,14 +1,16 @@
 import {
   DishesUpdates,
   dishesUpdatesSchema,
-  MenuUpdates,
-  menuUpdatesSchema,
   newMenuSchema,
   type NewMenu,
 } from "./validation";
 import { Repository } from ".";
 import { v4 } from "uuid";
-import { DuplicateKeyError, NotFoundError } from "../../errors";
+import {
+  BadRequestError,
+  DuplicateKeyError,
+  NotFoundError,
+} from "../../errors";
 
 export const createService = (db: Repository) => {
   return {
@@ -20,21 +22,26 @@ export const createService = (db: Repository) => {
       return menu;
     },
 
-    
-
     addMenu: async (rawData: NewMenu) => {
       const parsedMenu = newMenuSchema.parse(rawData);
-      const exists = (await db.getAll()).some((dbMenu) => dbMenu.name === parsedMenu.name);
+      const exists = (await db.getAll()).some(
+        (dbMenu) => dbMenu.name === parsedMenu.name,
+      );
       if (exists) throw new DuplicateKeyError("Menu name already exists.");
       db.create({ id: v4(), ...parsedMenu });
     },
 
     updateMenu: async (rawData: DishesUpdates, id: string) => {
-      const parsedUpdates = dishesUpdatesSchema.parse(rawData);      
-      const menu = await db.getById(id);
-      if (!menu) throw new NotFoundError("Menu not found.");
-      const newDishes = [...menu.dishes, ...(parsedUpdates.dishes ?? [])];
-      return await db.update({...menu, dishes: newDishes}, id);
+      const { dishes } = dishesUpdatesSchema.parse(rawData);
+      if (!dishes) throw new BadRequestError("Wrong input for updating menu.");
+
+      const menus = await db.getAll();
+      const index = menus.findIndex((menu) => menu.id === id);
+      if (index === -1) throw new NotFoundError("Menu not found.");
+
+      const dishesSet = new Set([...menus[index].dishes, ...dishes]);
+
+      return await db.update([...dishesSet], index);
     },
 
     removeMenu: async (id: string) => {
