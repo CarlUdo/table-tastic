2c30f50a1eff43f5861c15469d280c8519673f80
CarlUdo <carl@wbsweden.com>
2024-11-16 20:00:04 +0100

    Make reservations constants

---
+++ src/features/reservations/constants.ts
@@ -0,0 +1,4 @@
+export const RESERVATION_NOT_FOUND = "Reservation not found.";
+export const INVALID_RESERVATION = "Invalid reservation format.";
+export const FULLY_BOOKED = "Restaurant is fully booked.";
+export const NO_MENU = "No menu available for the given time.";
