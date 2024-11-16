3ef52c91b625419b9801c36cc565d877b27570ab
CarlUdo <carl@wbsweden.com>
2024-11-16 19:31:46 +0100

    Make Menu constants

---
+++ src/features/menus/constants.ts
@@ -0,0 +1,4 @@
+export const MENU_NOT_FOUND = "Menu not found.";
+export const MENU_EXISTS = "Menu name already exists.";
+export const MENU_INVALID = "Invalid menu format.";
+export const MENU_WRONG_INPUT = "Wrong input for updating menu.";
