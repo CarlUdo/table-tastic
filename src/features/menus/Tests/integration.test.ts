67e2e753caf900709d0fa81bd5a3389c153ad3d3
CarlUdo <carl@wbsweden.com>
2024-11-17 09:10:03 +0100

    Update menus integration tests

--- src/features/menus/Tests/integration.test.ts
+++ src/features/menus/Tests/integration.test.ts
@@ -1,20 +1,16 @@
-
 import request from "supertest";
 import { deepEqual, equal } from "node:assert/strict";
 import test from "node:test";
-import { createApp } from "../../../app";
-import { createRepository } from "../repository";
-import { createDatabase } from "../mock-db";
+import { createApp, initApp } from "../../../app";
+import { Menu } from "../validation";
+import { createMenusRepository } from "../repository";
 import { createMenusFeature } from "../feature";
-import type { Menu } from "../validation";
+import { createReservationsDatabase, createReservationsFeature, createReservationsRepository } from "../../reservations";
 
 const MENUS_BASE_URL = "/api/v1/menus";
 
-const db = createRepository(createDatabase());
-const menusRouter = createMenusFeature(db).router;
-
 test("GET all menus should return 3 menus", async () => {
-  const app = createApp(menusRouter);
+  const app = initApp();
 
   const response = await request(app).get(MENUS_BASE_URL);
 
@@ -25,7 +21,7 @@ test("GET all menus should return 3 menus", async () => {
 });
 
 test("GET menu by id should return the correct menu", async () => {
-  const app = createApp(menusRouter);
+  const app = initApp();
 
   const validId = "9de3faf7-36f3-4449-b4b5-7c3393f00e10";
 
@@ -55,9 +51,13 @@ test("POST a new menu should create and return the menu", async () => {
     },
   ];
 
-  const db = createRepository(initialMenus);
-  const menusRouter = createMenusFeature(db).router;
-  const app = createApp(menusRouter); 
+  const menusDb = createMenusRepository(initialMenus);
+  const menusRouter = createMenusFeature(menusDb).router;
+
+  const reservationsDb = createReservationsRepository(createReservationsDatabase());
+  const reservationsRouter = createReservationsFeature(reservationsDb).router;
+
+  const app = createApp(menusRouter, reservationsRouter);
 
   const newMenu = {
     name: "Breakfast Menu",
@@ -72,8 +72,8 @@ test("POST a new menu should create and return the menu", async () => {
   deepEqual(response.body.dishes, newMenu.dishes);
 });
 
-test("PUT should update an existing menu and return the updated menu", async () => {
-  const app = createApp(menusRouter);
+test("PATCH should update an existing menu and return the updated menu", async () => {
+  const app = initApp();
 
   const validId = "fd9c2bf1-8540-4ed9-9be7-155877262259";
 
@@ -97,7 +97,7 @@ test("PUT should update an existing menu and return the updated menu", async () 
 });
 
 test("DELETE a menu should remove the menu and return it", async () => {
-  const app = createApp(menusRouter);
+  const app = initApp();
 
   const validId = "42995559-2641-4d33-85e9-9043373fc6bf";
 
