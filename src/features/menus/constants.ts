58790cf7c649ff21e8b25e09c47ad1660f1d49a2
CarlUdo <carl@wbsweden.com>
2024-11-17 08:15:52 +0100

    Remove error constant

--- src/features/menus/constants.ts
+++ src/features/menus/constants.ts
@@ -1,4 +1,3 @@
 export const MENU_NOT_FOUND = "Menu not found.";
 export const MENU_EXISTS = "Menu name already exists.";
 export const MENU_INVALID = "Invalid menu format.";
-export const MENU_WRONG_INPUT = "Wrong input for updating menu.";
