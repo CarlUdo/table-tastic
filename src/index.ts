95defe86f004fd03d293fb297e276538790f7c61
CarlUdo <carl@wbsweden.com>
2024-11-17 09:08:34 +0100

    Reafactor main index file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
-import { createApp } from "./app/app";
-import { createMenusDatabase, createMenusFeature, createMenusRepository } from "./features/menus";
-import { createReservationsDatabase, createReservationsFeature, createReservationsRepository } from "./features/reservations";
+import { initApp } from "./app";
 
 (() => {
   const PORT = 3000;
   const HOST = "0.0.0.0";
 
-  const menusDb = createMenusRepository(createMenusDatabase());
-  const menusRouter = createMenusFeature(menusDb).router;
-
-  const reservationsDb = createReservationsRepository(createReservationsDatabase());
-  const reservationsRouter = createReservationsFeature(reservationsDb).router;
-  
-  const app = createApp(menusRouter, reservationsRouter);
+  const app = initApp();
 
   app.listen(PORT, HOST, () => {
     console.log(`Server is up an running on http://${HOST}:${PORT}`);
