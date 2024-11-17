a63f1b657b94981a6c0231cbe6cd05678d94a5a6
CarlUdo <carl@wbsweden.com>
2024-11-17 07:58:17 +0100

    Update reservation mock database

--- src/features/reservations/mock-db.ts
+++ src/features/reservations/mock-db.ts
@@ -8,7 +8,6 @@ export const createReservationsDatabase = (): Reservation[] => {
       tableNumber: 1,
       date: "2024-11-23",
       time: "08:00",
-      menuId: "9de3faf7-36f3-4449-b4b5-7c3393f00e10",
     },
     {
       id: "f29f33f2-d230-4901-93d8-7145baf409a7",
@@ -16,7 +15,6 @@ export const createReservationsDatabase = (): Reservation[] => {
       tableNumber: 6,
       date: "2024-12-10",
       time: "13:00",
-      menuId: "fd9c2bf1-8540-4ed9-9be7-155877262259",
     },
     {
       id: "b7f24239-4698-4efc-94e3-18eaa3503677",
@@ -24,7 +22,6 @@ export const createReservationsDatabase = (): Reservation[] => {
       tableNumber: 24,
       date: "2024-12-24",
       time: "19:00",
-      menuId: "42995559-2641-4d33-85e9-9043373fc6bf",
     },
   ];
 };
